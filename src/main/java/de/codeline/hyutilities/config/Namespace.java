00e2322df0c6160089beb26ed7fcff6422023ae1
Maximilian B. <codeline.request@gmail.com>
2026-01-21 19:31:58 +0100

    Added langNotFoundIssue

--- src/main/java/de/codeline/hyutilities/config/Namespace.java
+++ src/main/java/de/codeline/hyutilities/config/Namespace.java
@@ -1,7 +1,10 @@
 package de.codeline.hyutilities.config;
 
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.util.Config;
 import com.membercat.issuelib.IssueLib;
+import com.membercat.issuelib.api.issue.ConfigIssue;
+import com.membercat.issuelib.api.issue.IssueLevel;
 import com.membercat.issuelib.api.issue.IssueNamespace;
 import de.codeline.hyutilities.HyUtilities;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +15,8 @@ import org.jetbrains.annotations.NotNull;
 public class Namespace extends IssueNamespace {
 
     public static final Namespace INSTANCE = new Namespace(HyUtilities.LOGGER);
+
+    public final ConfigIssue langNotFoundIssue = this.add("HL0", IssueLevel.WARNING);
 
     private Namespace(HytaleLogger logger) {
         super("HyUtilities", IssueLib.basicEnv(t -> logger.atWarning().log(t.asString())));
