44a20ce770e4cf980473090b612a1bbbd41c30e4
Maximilian B. <codeline.request@gmail.com>
2026-01-21 19:32:15 +0100

    Checking if specified language is available

--- src/main/java/de/codeline/hyutilities/config/PluginConfig.java
+++ src/main/java/de/codeline/hyutilities/config/PluginConfig.java
@@ -1,7 +1,14 @@
 package de.codeline.hyutilities.config;
 
 import com.membercat.issuelib.api.annotation.Property;
+import com.membercat.issuelib.api.issue.IssueContext;
 import com.membercat.issuelib.api.loader.ConfigLoadable;
+import com.membercat.issuelib.api.loader.LoaderContext;
+import de.codeline.hyutilities.HyUtilities;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
 
 /**
  * PluginConfig (typically config.yml) defining the values of the main config
@@ -12,6 +19,16 @@ public class PluginConfig implements ConfigLoadable {
     public @Property EnabledFeaturesConfig features;
 
     public static class GeneralMainConfig implements ConfigLoadable {
+        @Override
+        public @Nullable Object onLoadProperty(@NotNull String key, @Nullable Object current, @NotNull Field field, IssueContext ctx, LoaderContext loaderCtx) {
+            if (!key.equals("language") || !(current instanceof String language)) return current;
+            if (HyUtilities.getLangManager().loadFile(language) == null) {
+                ctx.append(Namespace.INSTANCE.langNotFoundIssue);
+                return "en_US";
+            }
+            return current;
+        }
+
         public @Property String language = "en_US";
     }
 
