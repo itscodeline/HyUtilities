aeb9e260917af23a7b1659ae511895edcf75ca3d
Maximilian B. <codeline.request@gmail.com>
2026-01-21 19:32:25 +0100

    Updated HyUtilitiesCommand

--- src/main/java/de/codeline/hyutilities/commands/HyUtilitiesCommand.java
+++ src/main/java/de/codeline/hyutilities/commands/HyUtilitiesCommand.java
@@ -7,6 +7,8 @@ import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.arguments.system.OptionalArg;
 import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
 import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
@@ -14,56 +16,74 @@ import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import de.codeline.hyutilities.HyUtilities;
 import de.codeline.hyutilities.config.PluginConfig;
 import de.codeline.hyutilities.lang.LangManager;
+import io.netty.util.concurrent.CompleteFuture;
 import org.jetbrains.annotations.NotNull;
 
-public class HyUtilitiesCommand extends AbstractPlayerCommand {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+
+public class HyUtilitiesCommand extends AbstractCommandCollection {
 
     private final LangManager langManager;
     private final PluginConfig mainConfig;
     private final HyUtilities hyUtilities;
 
     public HyUtilitiesCommand(HyUtilities hyUtilities) {
-        super("hyutilities", "Primary command for using subcommands directly related to the HyUtilities plugin.", false);
-        this.langManager = hyUtilities.getLangManager();
+        super("hyutilities", "Primary command for using subcommands directly related to the HyUtilities plugin.");
+        this.langManager = HyUtilities.getLangManager();
         this.mainConfig = hyUtilities.getMainConfig();
         this.hyUtilities = hyUtilities;
+        this.addAliases("hy", "hyutils");
         this.addSubCommand(new ReloadCommand(hyUtilities));
-        this.addUsageVariant(this);
     }
 
-    @Override
-    protected void execute(@NotNull CommandContext commandContext, @NotNull Store<EntityStore> store, @NotNull Ref<EntityStore> ref, @NotNull PlayerRef playerRef, @NotNull World world) {
-        hyUtilities.reloadConfig();
-    }
+    public static class ReloadCommand extends AbstractAsyncCommand {
 
-    public static class ReloadCommand extends AbstractPlayerCommand {
-
-        RequiredArg<String> reloadableArg = this.withRequiredArg("reloadable", "Specify what should be reloaded (Options: 'config', 'lang')", ArgTypes.STRING);
+        RequiredArg<ReloadType> reloadableArg = this.withRequiredArg("reload_type", "Specify what should be reloaded",
+                ArgTypes.forEnum("ReloadType", ReloadType.class));
         private final LangManager langManager;
         private final HyUtilities hyUtilities;
 
         public ReloadCommand(HyUtilities hyUtilities) {
             super("reload", "Reload specific plugin configurations.", false);
-            langManager = hyUtilities.getLangManager();
+            langManager = HyUtilities.getLangManager();
             this.hyUtilities = hyUtilities;
         }
 
         @Override
-        protected void execute(@NotNull CommandContext commandContext, @NotNull Store<EntityStore> store, @NotNull Ref<EntityStore> ref, @NotNull PlayerRef playerRef, @NotNull World world) {
+        protected @NotNull CompletableFuture<Void> executeAsync(@NotNull CommandContext commandContext) {
+            try {
+                hyUtilities.createConfigFiles(hyUtilities.getDataDirectory());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             switch(reloadableArg.get(commandContext)) {
-                case "config":
-                    hyUtilities.reloadConfig();
-                    playerRef.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.config")));
-                    break;
-                case "lang":
+                case ReloadType.ALL:
+                    hyUtilities.reloadConfig(commandContext);
                     langManager.reload(hyUtilities.getMainConfig().general.language);
-                    playerRef.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.lang")));
+                    commandContext.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.all")));
+                    break;
+                case ReloadType.CONFIG:
+                    hyUtilities.reloadConfig(commandContext);
+                    commandContext.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.config")));
+                    break;
+                case ReloadType.LANG:
+                    langManager.reload(hyUtilities.getMainConfig().general.language);
+                    commandContext.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.lang")));
                     break;
                 default:
-                    playerRef.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.notFound")));
+                    commandContext.sendMessage(Message.raw(langManager.get("general.hyUtilities.reload.notFound")));
                     break;
             }
+            return CompletableFuture.completedFuture(null);
         }
+    }
+
+    private enum ReloadType {
+        ALL,
+        CONFIG,
+        LANG
     }
 
 }
